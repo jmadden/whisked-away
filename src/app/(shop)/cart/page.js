0898170ecf83b788109c74acad79e6d47b5d31cd
Jim Madden <jim@mindovermadden.net>
2025-12-30 14:42:29 -0800

    Place holders
    added place holders

---
+++ src/app/(shop)/cart/page.js
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Your Cart Page</h1>
+      <p>Baking tools, ingredients, and supplies.</p>
+    </main>
+  );
+}
---
+++ src/app/(shop)/products/page.js
@@ -0,0 +1,8 @@
+export default function ProductsPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Products Page</h1>
+      <p>Baking tools, ingredients, and supplies.</p>
+    </main>
+  );
+}
